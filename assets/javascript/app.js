7bec9cf4513fe6682964b451c5c90b7b4c0301d8
Mattylaw192 <mattylaw192@gmail.com>
2018-10-17 23:00:08 -0400

    created timer without visuals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,71 @@
-var timer = 0;
+
 var startButton;
 var correctAnswers = 0;
 var wrongAnswers = 0;
 
+
+
+function timeUp() {
+
+    $("#timerCountText").text("Time's Up!")
+    
+    };
+    
+
+
+var timer = {
+    start: function () {
+        alert("test");
+        setTimeout(timeUp,1000*5);
+        setInterval(function() {
+            $('.timerCountText').text((new Date - start) / 1000 + " Seconds");
+        }, 1000);
+
+    }
+
+    
+};
+
+$(".startButton").on("click", timer.start);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var questions = {
+//     "questionOne": [
+//         {
+//             "correct":"a man"
+//             "incorrect": [
+//                 "a squirrel",
+//                 "a houseplant",
+//                 "Hey now, you're an all star"
+//             ]
+//         }
+//     ]
+//     "questionTwo":[
+//         {}
+//     ]
+// }
+
+
+
+
+
+
+
+
+//quiz questions & Answers//
+// var questions {
+
+
+// // }
